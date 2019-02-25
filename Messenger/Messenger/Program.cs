d5279015ae1f3ddfd022910d9469c450b7752e0b
Aaron <aaron@xnovax.net>
2019-02-25 14:22:57 -0500

    Integrated Basic Functionality

--- Messenger/Messenger/Program.cs
+++ Messenger/Messenger/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace Messenger
 {
@@ -7,11 +11,32 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
+            string toNumber = "+1";
+            int messagePerNumber = 3;
+            
             StreamReader reader = new StreamReader("auth.txt");
             //Load Settings
-            string accountSid;
-            string authToken;
+            string accountSid = reader.ReadLine();
+            string authToken = reader.ReadLine();
+
+            TwilioClient.Init(accountSid, authToken);
             
+            
+            List<string> fromList = new List<string>();
+            fromList.Add("+1");
+
+            for (int i = 0; i < fromList.Count; i++)
+            {
+                for (int j = 0; j < messagePerNumber; j++)
+                {
+                    var message = MessageResource.Create(
+                        body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                        from: new Twilio.Types.PhoneNumber(fromList[i]),
+                        to: new Twilio.Types.PhoneNumber(toNumber)
+                        );
+                    Console.WriteLine(message.Sid);
+                }
+            }
         }
     }
 }
