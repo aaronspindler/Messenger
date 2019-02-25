ea2052b0ec09de7653c07a0eab85fcdd2caec515
Aaron <aaron@xnovax.net>
2019-02-25 14:33:27 -0500

    Added a message file so you can send multi line and a numbers file so you can send from more than one number

--- Messenger/Messenger/Program.cs
+++ Messenger/Messenger/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Threading;
+using Newtonsoft.Json.Linq;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -11,30 +13,45 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
-            string toNumber = "+1";
+            int interval = 2000;
+            
+            //Phone number format must follow + (Country Code) (Area Code) Number
+            // For instance +11234567890 where 1 is country code 123 is area code and then the number
+            string toNumber = "+";
             int messagePerNumber = 3;
             
-            StreamReader reader = new StreamReader("auth.txt");
+            StreamReader authReader = new StreamReader("auth.txt");
             //Load Settings
-            string accountSid = reader.ReadLine();
-            string authToken = reader.ReadLine();
+            string accountSid = authReader.ReadLine();
+            string authToken = authReader.ReadLine();
+            authReader.Close();
+            
+            StreamReader messageReader = new StreamReader("message.txt");
+            string messageBody = messageReader.ReadToEnd();
+            messageReader.Close();
+            
+            StreamReader numbersReader = new StreamReader("numbers.txt");
+            List<string> fromList = new List<string>();
+            string line = numbersReader.ReadLine();
+            do
+            {
+                fromList.Add(line);
+                line = numbersReader.ReadLine();
+            } while (line != null);
 
             TwilioClient.Init(accountSid, authToken);
-            
-            
-            List<string> fromList = new List<string>();
-            fromList.Add("+1");
 
             for (int i = 0; i < fromList.Count; i++)
             {
                 for (int j = 0; j < messagePerNumber; j++)
                 {
                     var message = MessageResource.Create(
-                        body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                        body: messageBody,
                         from: new Twilio.Types.PhoneNumber(fromList[i]),
                         to: new Twilio.Types.PhoneNumber(toNumber)
                         );
-                    Console.WriteLine(message.Sid);
+                    Console.WriteLine(DateTime.Now + message.Sid);
+                    Thread.Sleep(interval);
                 }
             }
         }
